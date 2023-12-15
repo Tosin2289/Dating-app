43af17c9fc1fa3e415250683bb83c061b273d398
Tosin2289 <ayoolatosin00@gmail.com>
2023-12-15 21:14:17 +0100

    assets

--- lib/views/authentication/login/login_screen.dart
+++ lib/views/authentication/login/login_screen.dart
@@ -10,6 +10,13 @@ class LoginScreen extends StatefulWidget {
 class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Center(
+            child: Column(
+          children: [],
+        )),
+      ),
+    );
   }
 }
---
+++ lib/views/utils/image.dart
@@ -0,0 +1,6 @@
+class DImages {
+  static String logo1 = "assets/logo.png";
+  static String logo2 = "assets/logo2.gif";
+  static String icon = "assets/icon.png";
+  static String profile = "assets/profile.png";
+}
