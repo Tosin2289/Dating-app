56631df4b9258c0ae73dd3ecea6c8dcb4be99239
Tosin2289 <ayoolatosin00@gmail.com>
2023-12-17 16:40:47 +0100

    login screen ui

--- lib/views/authentication/login/login_screen.dart
+++ lib/views/authentication/login/login_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../../utils/image.dart';
+import '../widgets/text_field_widgets.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -10,29 +11,102 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
+  bool showProgressbar = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Image(
-              image: AssetImage(
-                DImages.logo1,
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image(
+                image: AssetImage(
+                  DImages.logo1,
+                ),
               ),
-            ),
-            const SizedBox(
-              height: 20,
-            ),
-            const Text("Welcome",
-                style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
-            const SizedBox(
-              height: 10,
-            ),
-            const Text("Login to find find your best match",
-                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
-          ],
+              const SizedBox(
+                height: 20,
+              ),
+              const Text("Welcome",
+                  style: TextStyle(fontSize: 22, fontWeight: FontWeight.bold)),
+              const SizedBox(
+                height: 10,
+              ),
+              const Text("Login to find find your best match",
+                  style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold)),
+              const SizedBox(
+                height: 28,
+              ),
+              CustomTextField(
+                controller: emailController,
+                labelText: 'Email',
+                iconData: Icons.email_outlined,
+                isObscured: false,
+              ),
+              const SizedBox(
+                height: 10,
+              ),
+              CustomTextField(
+                controller: passwordController,
+                labelText: 'Password',
+                iconData: Icons.lock_outlined,
+                isObscured: true,
+              ),
+              const SizedBox(
+                height: 20,
+              ),
+              SizedBox(
+                height: 55,
+                width: double.infinity,
+                child: ElevatedButton(
+                  style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.white,
+                      foregroundColor: Colors.black,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10))),
+                  onPressed: () {},
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+              const SizedBox(
+                height: 16,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "Don't have an account?",
+                    style: TextStyle(fontSize: 16, color: Colors.grey),
+                  ),
+                  TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Register here",
+                        style: TextStyle(
+                            fontSize: 16,
+                            color: Colors.white,
+                            fontWeight: FontWeight.bold),
+                      )),
+                  const SizedBox(
+                    height: 16,
+                  ),
+                  showProgressbar == true
+                      ? const CircularProgressIndicator(
+                          valueColor:
+                              AlwaysStoppedAnimation<Color>(Colors.pink),
+                        )
+                      : const SizedBox()
+                ],
+              ),
+            ],
+          ),
         ),
       ),
     );
