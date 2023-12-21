14eddb4ab46d48c811f65c2da2d21ab8b612d7a5
Tosin2289 <ayoolatosin00@gmail.com>
2023-12-21 05:46:44 +0100

    upload image url, user creation, and storing data in firestore

--- lib/controllers/auth/authentication_controller.dart.dart
+++ lib/controllers/auth/authentication_controller.dart.dart
@@ -1,5 +1,10 @@
 import 'dart:io';
 
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dating_app/models/person.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
 
@@ -14,5 +19,65 @@ class AuthenticationController extends GetxController {
       Get.snackbar("Hurry", "You have sucessfully picked your profile image");
     }
     pickedFile = Rx<File?>(File(imageFile!.path));
+  }
+
+  Future<String> uploadImageToStorage(File imageFile) async {
+    Reference referenceStorage = FirebaseStorage.instance
+        .ref()
+        .child("Profile Images")
+        .child(FirebaseAuth.instance.currentUser!.uid);
+    UploadTask task = referenceStorage.putFile(imageFile);
+    TaskSnapshot snapshot = await task;
+    String downloadUrlOfImage = await snapshot.ref.getDownloadURL();
+    return downloadUrlOfImage;
+  }
+
+  createNewUserAccount(
+    File imageProfile,
+    String email,
+    String password,
+    String name,
+    String age,
+    String phoneNo,
+    String city,
+    String country,
+    String lookingForInaPartner,
+    String height,
+    String weight,
+    String bodyType,
+    String drink,
+    String smaoke,
+    String maritalStatus,
+    String haveChildren,
+    String noOfChildren,
+    String profession,
+    String employmentStatus,
+    String income,
+    String livingSituation,
+    String willingToRelocate,
+    String relationshipLookingFor,
+    String nationality,
+    String education,
+    String language,
+    String religion,
+    String ethnicity,
+  ) async {
+    try {
+      UserCredential credential = await FirebaseAuth.instance
+          .createUserWithEmailAndPassword(email: email, password: password);
+      String urlOfDownloadedImage = await uploadImageToStorage(imageProfile);
+      await FirebaseFirestore.instance
+          .collection("Users")
+          .doc(FirebaseAuth.instance.currentUser!.uid)
+          .set(Person().toMap());
+      Get.snackbar("Hurry", "Your account has been created sucessfully",
+          backgroundColor: Colors.pink, colorText: Colors.white);
+    } on FirebaseAuthException catch (e) {
+      Get.snackbar("Oh Snap", "Error occured :$e",
+          backgroundColor: Colors.red, colorText: Colors.white);
+    } catch (e) {
+      Get.snackbar("Oh Snap", "Error occured :$e",
+          backgroundColor: Colors.red, colorText: Colors.white);
+    }
   }
 }
---
+++ lib/models/person.dart
@@ -0,0 +1,139 @@
+class Person {
+  //Personal info
+  String? imageProfile;
+  String? email;
+  String? password;
+  String? name;
+  String? age;
+  String? phoneNo;
+  String? city;
+  String? country;
+  String? profileHeading;
+  String? lookingForInaPartner;
+  int? publishedDateTime;
+  //Appearance
+  String? height;
+  String? weight;
+  String? bodyType;
+
+  //LifeStyle
+  String? drink;
+  String? smaoke;
+  String? maritalStatus;
+  String? haveChildren;
+  String? noOfChildren;
+  String? profession;
+  String? employmentStatus;
+  String? income;
+  String? livingSituation;
+  String? willingToRelocate;
+  String? relationshipLookingFor;
+
+  //Background
+  String? nationality;
+  String? education;
+  String? language;
+  String? religion;
+  String? ethnicity;
+  Person({
+    this.imageProfile,
+    this.email,
+    this.password,
+    this.name,
+    this.age,
+    this.phoneNo,
+    this.city,
+    this.country,
+    this.profileHeading,
+    this.lookingForInaPartner,
+    this.publishedDateTime,
+    this.height,
+    this.weight,
+    this.bodyType,
+    this.drink,
+    this.smaoke,
+    this.maritalStatus,
+    this.haveChildren,
+    this.noOfChildren,
+    this.profession,
+    this.employmentStatus,
+    this.income,
+    this.livingSituation,
+    this.willingToRelocate,
+    this.relationshipLookingFor,
+    this.nationality,
+    this.education,
+    this.language,
+    this.religion,
+    this.ethnicity,
+  });
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'imageProfile': imageProfile,
+      'email': email,
+      'password': password,
+      'name': name,
+      'age': age,
+      'phoneNo': phoneNo,
+      'city': city,
+      'country': country,
+      'profileHeading': profileHeading,
+      'lookingForInaPartner': lookingForInaPartner,
+      'publishedDateTime': publishedDateTime,
+      'height': height,
+      'weight': weight,
+      'bodyType': bodyType,
+      'drink': drink,
+      'smaoke': smaoke,
+      'maritalStatus': maritalStatus,
+      'haveChildren': haveChildren,
+      'noOfChildren': noOfChildren,
+      'profession': profession,
+      'employmentStatus': employmentStatus,
+      'income': income,
+      'livingSituation': livingSituation,
+      'willingToRelocate': willingToRelocate,
+      'relationshipLookingFor': relationshipLookingFor,
+      'nationality': nationality,
+      'education': education,
+      'language': language,
+      'religion': religion,
+      'ethnicity': ethnicity,
+    };
+  }
+
+  factory Person.fromMap(Map<String, dynamic> map) {
+    return Person(
+      imageProfile: map['imageProfile'] ?? '',
+      email: map['email'] ?? '',
+      password: map['password'] ?? '',
+      name: map['name'] ?? '',
+      age: map['age'] ?? '',
+      phoneNo: map['phoneNo'] ?? '',
+      city: map['city'] ?? '',
+      country: map['country'] ?? '',
+      profileHeading: map['profileHeading'] ?? '',
+      lookingForInaPartner: map['lookingForInaPartner'] ?? '',
+      publishedDateTime: map['publishedDateTime'] ?? '',
+      height: map['height'] ?? '',
+      weight: map['weight'] ?? '',
+      bodyType: map['bodyType'] ?? '',
+      drink: map['drink'] ?? '',
+      smaoke: map['smaoke'] ?? '',
+      maritalStatus: map['maritalStatus'] ?? '',
+      haveChildren: map['haveChildren'] ?? '',
+      noOfChildren: map['noOfChildren'] ?? '',
+      profession: map['profession'] ?? '',
+      employmentStatus: map['employmentStatus'] ?? '',
+      income: map['income'] ?? '',
+      livingSituation: map['livingSituation'] ?? '',
+      willingToRelocate: map['willingToRelocate'] ?? '',
+      relationshipLookingFor: map['relationshipLookingFor'] ?? '',
+      nationality: map['nationality'] ?? '',
+      education: map['education'] ?? '',
+      language: map['language'] ?? '',
+      religion: map['religion'] ?? '',
+      ethnicity: map['ethnicity'] ?? '',
+    );
+  }
+}
