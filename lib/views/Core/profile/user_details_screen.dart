b4477ceb6374c4c0f76726ed4ae7bac1df68a18f
Tosin2289 <ayoolatosin00@gmail.com>
2023-12-22 23:42:46 +0100

    logout

--- lib/views/Core/profile/user_details_screen.dart
+++ lib/views/Core/profile/user_details_screen.dart
@@ -1,3 +1,5 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 class UserDetailsScreen extends StatefulWidget {
@@ -11,7 +13,20 @@ class _UserDetailsScreenState extends State<UserDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
+      appBar: AppBar(
+          centerTitle: true,automaticallyImplyLeading: false,
+          actions: [
+            IconButton(
+                onPressed: () {
+                  FirebaseAuth.instance.signOut();
+                },
+                icon: const Icon(Icons.logout))
+          ],
+          title: const Text(
+            "Profile Name",
+            style: TextStyle(color: Colors.white),
+          )),
+      body: const Center(
           child: Text(
         "User Details Screen",
         style: TextStyle(fontSize: 30),
