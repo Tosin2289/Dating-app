2b6c0a72e7a7494e21361683337da08d6a65f594
Tosin2289 <ayoolatosin00@gmail.com>
2023-12-24 22:31:05 +0100

    swipper screen

--- lib/views/Core/swipe/swipe_screen.dart
+++ lib/views/Core/swipe/swipe_screen.dart
@@ -16,31 +16,141 @@ class _SwippingScreenState extends State<SwippingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    print("Over Here as well");
-    print(profileController.usersProfileList.length);
     return Scaffold(
+        appBar: AppBar(
+          automaticallyImplyLeading: false,
+          backgroundColor: Colors.transparent,
+          elevation: 0,
+          actions: [
+            IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.filter_list,
+                  color: Colors.white,
+                )),
+          ],
+        ),
         body: Obx(
-      () => PageView.builder(
-        itemCount: profileController.usersProfileList.length,
-        controller: PageController(initialPage: 0, viewportFraction: 1),
-        itemBuilder: (context, index) {
-          final eachProfileInfo = profileController.usersProfileList[index];
+          () => CardSwiper(
+            cardsCount: profileController.usersProfileList.length,
+            numberOfCardsDisplayed: 3,
+            backCardOffset: const Offset(40, 40),
+            padding: const EdgeInsets.all(24.0),
+            cardBuilder: (context, index, x, y) {
+              final eachProfileInfo = profileController.usersProfileList[index];
 
-          return DecoratedBox(
-            decoration: BoxDecoration(
-              image: DecorationImage(
-                  image: NetworkImage(eachProfileInfo.imageProfile.toString()),
-                  fit: BoxFit.cover),
-            ),
-            child: Center(
-                child: Text(eachProfileInfo.name.toString(),
-                    style: const TextStyle(
-                      color: Colors.white,
-                      fontSize: 30,
-                    ))),
-          );
-        },
-      ),
-    ));
+              return DecoratedBox(
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(15),
+                  image: DecorationImage(
+                      image:
+                          NetworkImage(eachProfileInfo.imageProfile.toString()),
+                      fit: BoxFit.cover),
+                ),
+                child: Padding(
+                  padding: const EdgeInsets.all(12.0),
+                  child: Center(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        GestureDetector(
+                          onTap: () {},
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                eachProfileInfo.name.toString(),
+                                style: const TextStyle(
+                                    color: Colors.white,
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold),
+                              ),
+                              Text(
+                                '${eachProfileInfo.age}•${eachProfileInfo.city}',
+                                style: const TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 16,
+                                ),
+                              ),
+                              const SizedBox(
+                                height: 5,
+                              ),
+                              Row(
+                                children: [
+                                  ElevatedButton(
+                                      onPressed: () {},
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.white30,
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(16)),
+                                      ),
+                                      child: Text(
+                                        eachProfileInfo.profession.toString(),
+                                        style: const TextStyle(
+                                            color: Colors.white, fontSize: 14),
+                                      )),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  ElevatedButton(
+                                      onPressed: () {},
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.white30,
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(16)),
+                                      ),
+                                      child: Text(
+                                        eachProfileInfo.religion.toString(),
+                                        style: const TextStyle(
+                                            color: Colors.white, fontSize: 14),
+                                      )),
+                                ],
+                              ),
+                              Row(
+                                children: [
+                                  ElevatedButton(
+                                      onPressed: () {},
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.white30,
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(16)),
+                                      ),
+                                      child: Text(
+                                        eachProfileInfo.country.toString(),
+                                        style: const TextStyle(
+                                            color: Colors.white, fontSize: 14),
+                                      )),
+                                  const SizedBox(
+                                    width: 10,
+                                  ),
+                                  ElevatedButton(
+                                      onPressed: () {},
+                                      style: ElevatedButton.styleFrom(
+                                        backgroundColor: Colors.white30,
+                                        shape: RoundedRectangleBorder(
+                                            borderRadius:
+                                                BorderRadius.circular(16)),
+                                      ),
+                                      child: Text(
+                                        eachProfileInfo.ethnicity.toString(),
+                                        style: const TextStyle(
+                                            color: Colors.white, fontSize: 14),
+                                      )),
+                                ],
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              );
+            },
+          ),
+        ));
   }
 }
