a1dc5150bf559ad72f7cd607289888164ecd359b
Tosin2289 <ayoolatosin00@gmail.com>
2023-12-28 04:45:33 +0100

    notification

---
+++ lib/pushNotficationSystem/push_notification_system.dart
@@ -0,0 +1,30 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:flutter/material.dart';
+
+class PushNotificationSystem {
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+  Future whenNotififcationRecived(BuildContext context) async {
+    FirebaseMessaging.instance
+        .getInitialMessage()
+        .then((RemoteMessage? remoteMessage) {
+      if (remoteMessage != null) {
+        openAppshowNotificationData(remoteMessage.data["userID"],
+            remoteMessage.data["SenderID"], context);
+      }
+    });
+    FirebaseMessaging.onMessage.listen((RemoteMessage? remoteMessage) {
+      if (remoteMessage != null) {
+        openAppshowNotificationData(remoteMessage.data["userID"],
+            remoteMessage.data["SenderID"], context);
+      }
+    });
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage? remoteMessage) {
+      if (remoteMessage != null) {
+        openAppshowNotificationData(remoteMessage.data["userID"],
+            remoteMessage.data["SenderID"], context);
+      }
+    });
+  }
+
+  openAppshowNotificationData(receiverID, senderID, context) async {}
+}
